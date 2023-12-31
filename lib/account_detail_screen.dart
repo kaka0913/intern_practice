4d9783630a5a1889c27ce2a931855920d458cb69
kaka0913 <kabuyuu0913@gmail.com>
2023-12-31 19:38:21 +0900

    import drawer

--- lib/account_detail_screen.dart
+++ lib/account_detail_screen.dart
@@ -6,9 +6,45 @@ class AccountDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Detail'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Drawer Demo'),
+      ),
+      drawer: Drawer(
+        //①appbar:と同階層に配置
+        child: ListView(
+          //②child:としてListViewを配置
+          padding: EdgeInsets.zero,
+          children: const <Widget>[
+            //③ListViewのchidrenにはHeaderをひとつ、子要素を複数個配置。
+            DrawerHeader(
+              //　ここでのHeader：DrawerHeader()を入れて、
+              decoration: BoxDecoration(
+                color: Colors.orange,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              //　子要素としてはListTileを入れる
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
       ),
     );
   }
